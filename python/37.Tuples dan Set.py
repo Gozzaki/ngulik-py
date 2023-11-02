59ae208b675d2ee39a9e9917b563e0b189d40a93
gozali <gozaliatamimi0@gmail.com>
2023-11-02 23:42:28 +0700

    37/71 Ngulik Py

---
+++ python/37.Tuples dan Set.py
@@ -0,0 +1,24 @@
+
+#list 
+data_list = [1,2,3,4,5]
+print(data_list)
+
+
+# Tuples tidak bisa di rubah isi nya
+data_tuples = (6,7,8,9)
+print(data_tuples)
+
+print(data_tuples[1])
+
+# tidak bisa d lakukan
+# data_tuples[1] = "ucup"
+# data_tuples.append()
+
+#sets tidak punya index hanya himpunan angka
+data_sets = {1,2,3,4,5}
+print(data_sets)
+# print(data_sets[0])
+
+
+
+
