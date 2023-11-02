f0c8cc8f09e107119bb093229a73d415476d9f25
gozali <gozaliatamimi0@gmail.com>
2023-11-02 23:45:32 +0700

    38/71 Ngulik Py

---
+++ python/38.Dictionary.py
@@ -0,0 +1,23 @@
+# list -> array ,mengakses data dengan menggunakan index
+
+data_list = ["gozali","gozzaki","zakia"]
+print(data_list[1])
+
+# Dictionary (dict) -> associative array
+# identifier -> key
+# data_dict = {
+#     'key1':'value1',
+#     'key2':'value2'
+# }
+# memanggil value dengan key /pengganti index
+data_dict = {
+    'ali':'gozali',
+    'zaki':'zakia',
+    'list' : data_list,
+    'no' : 17
+}
+print(data_dict['ali'])
+print(data_dict['list'])
+print(data_dict['no'])
+
+
