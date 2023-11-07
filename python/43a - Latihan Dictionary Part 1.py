97478beb5de8328bda6d5866260c63dfd6901c4e
gozali <gozaliatamimi0@gmail.com>
2023-11-07 13:43:17 +0700

    43b/71 Ngulik Py

--- python/43a - Latihan Dictionary Part 1.py
+++ python/43a - Latihan Dictionary Part 1.py
@@ -10,9 +10,9 @@ mahasiswa_template = {
     'sks_lulus' : 21,
     'lahir' : datetime.datetime(2003,6,17)
 }
+data_mahasiswa = {}
 while True:
     os.system("cls")
-    data_mahasiswa = {}
     print(f"{'SELAMAT DATANG':^20}")
     print(f"{'DATA MAHASISWA':^20}")
     print(f"{'-'*20:^20}")
@@ -30,17 +30,17 @@ while True:
 
     KEY = ''.join((random.choice(string.ascii_uppercase) for i in range(6)))
     data_mahasiswa.update({KEY:mahasiswa})
-    print(f"\n{'KEY':<6} {'nama':<17} {'SKS':<5} {'lahir':<10}")
+    print(f"\n{'KEY':<6} {'nama':<17} {'nim':<6} {'SKS':<5} {'lahir':<10}")
 
-    for mahasiswa in data_mahasiswa:
-        KEY = mahasiswa
+    for i in data_mahasiswa:
+        KEY = i
         NAMA = data_mahasiswa[KEY]['nama']
         NIM = data_mahasiswa[KEY]['nim']
         SKS = data_mahasiswa[KEY]['sks_lulus']
         LAHIR = data_mahasiswa[KEY]['lahir'].strftime("%x")
-        print(f"{KEY:<6} {NAMA:<17} {SKS:<5} {LAHIR:<10}")
+        print(f"{KEY:<6} {NAMA:<17} {NIM:<6} {SKS:<5} {LAHIR:<10}")
 
-        print("\n")
+    print("\n")
     is_done = input("Mau Tambah Lagi BRO (y/n)")
     if is_done == "n":
         break
