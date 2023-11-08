cf1e5e0869b83129bb68cb1d64fc9637ffaa53c6
gozali <gozaliatamimi0@gmail.com>
2023-11-08 15:05:05 +0700

    40/71 Ngulik Py

--- python/40 - Loop Dictionary.py
+++ python/40 - Loop Dictionary.py
@@ -18,8 +18,8 @@ print(f"\n{10*'='} hanya mengambil key\n")
 keys = teman_teman.keys() #untuk mengambil keys nya saja dalam dictionary
 print(keys)
 
-for key in teman_teman.keys():
-    print(teman_teman.get(key))
+for i in teman_teman.keys():
+    print(teman_teman.get(i))
 
 print(f"\n{10*'='} hanya mengambil value\n")
 values = teman_teman.values() # untuk mengambil value nya saja
@@ -27,3 +27,12 @@ print(values)
 
 for value in teman_teman.values():
     print(values)
+print("=================== items\n")
+items = teman_teman.items()
+print(items)
+
+for item in teman_teman.items():
+	print(item)
+
+for key,value in teman_teman.items():
+	print(f"key = {key}, value = {value}")
